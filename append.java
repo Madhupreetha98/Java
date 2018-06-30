aa2d34e77aec4e51c918d041442b7d8b2a8edfef
Madhupreetha98 <36624004+Madhupreetha98@users.noreply.github.com>
2018-06-30 20:50:24 +0530

    append.java

---
+++ append.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+class Ap{
+public static void main(String args[]){
+Scanner sc=new Scanner(System.in);
+String s=sc.nextLine();
+StringBuffer s1=new StringBuffer(s);
+
+System.out.println(s1.append("."));
+}
+}
